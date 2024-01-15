90d1be8972390e0910173a62781b4043a3c4316c
Dinesh Vasireddy <dineshvasireddy@college.harvard.edu>
2024-01-15 01:47:58 -0600

    contact_update

--- components/Header/index.js
+++ components/Header/index.js
@@ -174,7 +174,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:dineshvasireddy@college.harvard.edu")}>
               Contact
             </Button>
 
